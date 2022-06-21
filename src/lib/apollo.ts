7dfad9f4357718f44ea2ab92723ab21e044a96ae
Brulishi <Bruli_pablo@hotmail.com>
2022-06-21 17:42:30 -0300

    [Feature Pablo Task:Primeira aula completa]

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,5 +1,6 @@
-import { ApolloClient } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri:''
+    uri:'https://api-sa-east-1.graphcms.com/v2/cl4okwcv50b6001xtfxpw06r6/master',
+    cache: new InMemoryCache()
 })
